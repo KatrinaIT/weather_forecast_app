cf26118c1ac7fe19a6602aff572dd23ac1ef7fbe
KatrinaIT <79994321+KatrinaIT@users.noreply.github.com>
2024-05-13 20:53:52 -0600

    injected forecast with JS

--- src/script.js
+++ src/script.js
@@ -19,7 +19,7 @@ function updateWeather(response) {
   timeElement.innerHTML = formatDate(date);
 
   let iconElement = document.querySelector("#icon");
-	iconElement.innerHTML = `<img
+  iconElement.innerHTML = `<img
                 class="weather-icon"
                 id="weather-icon"
                 src="${response.data.condition.icon_url}"
@@ -28,33 +28,70 @@ function updateWeather(response) {
 }
 
 function formatDate(date) {
-	
-	let hours = date.getHours();
-	let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
 
-	let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-	let day = days[date.getDay()];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
-	if(minutes < 10) {
-		minutes = `0${minutes}`;
-	}
-	return `${day} ${hours}:${minutes}`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
-let apiKey = "1ad00cd57fae02a14f050bfc3o387tf2";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-axios.get(apiUrl).then(updateWeather);
-};
+  let apiKey = "1ad00cd57fae02a14f050bfc3o387tf2";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
 
-function handleForm (event){
-	event.preventDefault();
-let searchInput = document.querySelector("#form-input");
+function handleForm(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#form-input");
 
-searchCity(searchInput.value);
-};
+  searchCity(searchInput.value);
+}
+
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+			<div class="row">
+				<div class="col-2 forecast-item">
+					<div class="weather-forecast-day">${day}</div>
+					<img
+						src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-night.png"
+						alt="forecast-icon"
+						width="40"
+					/>
+					<div class="weather-forecast-temp">
+						<span class="weather-forecast-temp-max">22°</span>
+						<span class="weather-forecast-temp-min"> 10°</span>
+					</div>
+				</div>
+			</div>
+		`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 
 let headerFormElement = document.querySelector("#header-form");
-headerFormElement.addEventListener('submit', handleForm);
+headerFormElement.addEventListener("submit", handleForm);
 
 searchCity("Ottawa");
+displayForecast();
